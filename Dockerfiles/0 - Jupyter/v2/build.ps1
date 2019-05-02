7222e6bcaf830d30c6da1655d4e7900405f63a59
Joao E. Arzamendia <jearzamendia@gmail.com>
2019-05-02 06:28:20 -0400

    new

--- Dockerfiles/0 - Jupyter/v2/build.ps1
+++ Dockerfiles/0 - Jupyter/v2/build.ps1
@@ -8,6 +8,5 @@ $date = [Xml.XmlConvert]::ToString((get-date),[Xml.XmlDateTimeSerializationMode]
 #build
 #docker build --build-arg BUILD_DATE=$date --build-arg http_proxy=http://proxy.sgi.ms.gov.br:8081 --build-arg https_proxy=http://proxy.sgi.ms.gov.br:8081 -t $release .
 docker build --build-arg BUILD_DATE=$date -t $release .
-
 #push
-docker push $release
+#docker push $release
